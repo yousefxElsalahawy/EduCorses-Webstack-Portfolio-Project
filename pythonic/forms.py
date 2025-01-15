97a29b264e3281c87fd368a121c0f65eb7447d3b
Yousef <yousef@example.com>
2025-01-15 22:03:31 +0200

    Add user registration and login validation
    - Implemented custom validation for username and email in registration form.
    - Added checks to ensure unique usernames and emails in the database.
    - Enhanced registration form with password validation using regex for stronger password requirements.

--- pythonic/forms.py
+++ pythonic/forms.py
@@ -1,7 +1,16 @@
 from tokenize import String
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms.validators import (
+    DataRequired,
+    Length,
+    Email,
+    Regexp,
+    EqualTo,
+    ValidationError,
+)
+
+from pythonic.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -26,6 +35,18 @@ class RegistrationForm(FlaskForm):
         "Confirm Password", validators=[DataRequired(), EqualTo("password")]
     )
     submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                "Username already exists! Please chosse a different one"
+            )
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email already exists! Please chosse a different one")
 
 
 class LoginForm(FlaskForm):
