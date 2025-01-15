309e804365bfcd4375063a9ac740c54d8afc7184
Yousef <yousef@example.com>
2025-01-15 19:48:18 +0200

    Enhance Flask application with dynamic data and templates
    - Replaced static HTML responses with dynamic templates using ender_template.
    - Introduced lessons and courses data to simulate content rendering from a database.
    - Updated the home route to display lessons and courses dynamically in the 'home.html' template.
    - Enhanced the bout route to use a dedicated 'about.html' template with dynamic title support.
    - Improved project structure to prepare for future scalability and better content organization.

--- pythonic.py
+++ pythonic.py
@@ -1,17 +1,86 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+lessons = [{
+    'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+]
+
+courses = [
+{
+        'name': 'Python',
+        'icon': 'python.svg',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+    {
+        'name': 'Data Analysis',
+        'icon': 'analysis.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+    {
+        'name': 'Machine Learning',
+        'icon': 'machine-learning.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Web Design',
+        'icon': 'web.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Blockchain',
+        'icon': 'blockchain.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Tips & Tricks',
+        'icon': 'idea.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+]
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1> Home page </h1>"
-
+    return render_template('home.html', lessons=lessons, courses=courses)
 
 @app.route("/about")
 def about():
-    return "<h1> About page </h1>"
+    return render_template('about.html', title="About")
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
